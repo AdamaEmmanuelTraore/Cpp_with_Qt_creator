5e11665ee059bd1244fbd8712d6058f5eb030f03
TraoreAdamaEmmanuel <adama.emmanuel.traore.11@gmail.com>
2025-07-22 00:42:35 +0200

    leçon sur les boucles

---
+++ les_boucles/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // la boucle while
+    while(false) cout << "Bonjour!" << endl; // Ici, Bonjour! ne s'affichera jamais à l'écran.
+
+    // la boucle do...while
+    do
+        cout << "Bonjour! de do...while \n" << endl;
+    while(false);                           // Alors que ici, Bonjour! s'affichera une seule fois.
+
+    // la boucle for
+    for (int i = 0; i < 30; ++i) {
+        cout << "Bonjour!" << endl;
+    }
+
+    return 0;
+}
